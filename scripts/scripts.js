d6aab19198f27939bdd96a5883ad113a9b457687
Michael Sandula <msandula@gmail.com>
2016-02-16 15:13:43 -0500

    Initial commit

---
+++ scripts/scripts.js
@@ -0,0 +1,22 @@
+$('header').hide().fadeIn(800);
+
+$("li").hover(
+	function() {
+		$(this).fadeTo(0, 0.25);
+	},
+	function() {
+		$(this).fadeTo(0, 0.5);
+	}
+);
+
+$("li").click(function() {
+	$(this).toggleClass("toggleColor");
+});
+
+$("a").click(function() {
+	$(this).text("Clicked");
+});
+
+$("#jqBonus").click(function() {
+	$(this).toggleClass("bonus");
+});
