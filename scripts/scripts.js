1eea72ed98629f16f7a10847485d5b10d606620b
Michael Sandula <msandula@gmail.com>
2016-02-20 16:22:34 -0500

    Added scroll to top feature

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,19 +1,40 @@
-// fade in background image
-$("header").hide().fadeIn(800);
+$(document).ready(function() {
+	
+	// scrolling down from top makes scroll-to-top arrow appear
+	var link,
+		toggleScrollToTop = function(){
+			if ($("body").scrollTop() > 600 || $("html").scrollTop() > 600) {
+				link.fadeIn(400);
+			} else {
+				link.fadeOut(400);
+			}
+		};
+	link = $(".scrollToTop");
+	$(window).scroll(toggleScrollToTop);
+	
+	// click activates scroll to top function
+	link.on("click", function() {
+		$("body").animate({scrollTop: 0});
+		$("html").animate({scrollTop: 0});
+	});
 
-// toggle blog archive headlines
-$("#oldEntries").on("click", function() {
-	$(".archives").slideToggle();
-});
+	// fade in background image
+	$("header").hide().fadeIn(800);
 
-// toggle blog one
-$("#blogOneHead").on("click", function() {
-	$("#blogOne").slideToggle();
-	$("#blogOneHead>span").toggle();
-});
+	// toggle blog archive headlines
+	$("#oldEntries").on("click", function() {
+		$(".archives").slideToggle();
+	});
 
-// toggle blog two
-$("#blogTwoHead").on("click", function() {
-	$("#blogTwo").slideToggle();
-	$("#blogTwoHead>span").toggle();
+	// toggle blog one
+	$("#blogOneHead").on("click", function() {
+		$("#blogOne").slideToggle();
+		$("#blogOneHead>span").toggle();
+	});
+
+	// toggle blog two
+	$("#blogTwoHead").on("click", function() {
+		$("#blogTwo").slideToggle();
+		$("#blogTwoHead>span").toggle();
+	});
 });
